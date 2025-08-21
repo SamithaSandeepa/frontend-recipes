cd9c0bb952e42a9eb4bfe52ada86bb56e9cf944b
Samitha Perera <73548027+SamithaSandeepa@users.noreply.github.com>
2025-08-21 23:32:27 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     domains: ["www.themealdb.com"], // 👈 whitelist external image host
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://62.146.182.119:4003/api/:path*",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
